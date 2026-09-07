e6279b0d5a2dcfb0ac84579c95ddc4f030b940c6
fathur wiriansyah <fathur.wiriansyah@satnusa.com>
2026-09-07 14:37:55 +0700

    Backup V2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
@@ -12,20 +13,20 @@ export default defineConfig(() => {
       },
     },
     server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+      hmr: env.DISABLE_HMR !== 'true',
+      watch: env.DISABLE_HMR === 'true' ? null : {},
       proxy: {
         '/portal-api': {
-          target: process.env.VITE_PORTAL_API_URL || 'http://localhost:3001',
+          target: env.VITE_PORTAL_API_URL || 'http://localhost:3001',
           changeOrigin: true,
           secure: false,
         },
         '/api': {
-          target: process.env.VITE_JUMPSERVER_URL || 'http://192.168.56.100',
+          target: env.VITE_JUMPSERVER_URL || 'http://192.168.56.100',
           changeOrigin: true,
           secure: false,
-        }
-      }
+        },
+      },
     },
-  };
-});
+  }
+})
